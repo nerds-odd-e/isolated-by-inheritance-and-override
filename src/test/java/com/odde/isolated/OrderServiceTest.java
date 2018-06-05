d5a839ac7cb75ed520ba1569972a4a8f6d77644d
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-06-05 11:58:36 +0800

    create the exercise code and test

---
+++ src/test/java/com/odde/isolated/OrderServiceTest.java
@@ -0,0 +1,14 @@
+package com.odde.isolated;
+
+import org.junit.Test;
+
+public class OrderServiceTest {
+    
+    @Test
+    public void syncbookorders_3_orders_only_2_book_order() {
+        // hard to isolate dependency to unit test
+
+        //OrderService target = new OrderService();
+        //target.SyncBookOrders();
+    }
+}
